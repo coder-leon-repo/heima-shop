51a33c4ae8c728e114eafb5005155b18b8e8e6c6
Leon <geek_leon@outlook.com>
2024-05-25 19:59:18 +0800

    feat:自定义导航栏

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,13 @@
-<script setup lang="ts">
-//
-</script>
-
 <template>
-  <view class="index">index</view>
+  <view class="index">
+    <CustomNavbar />
+    <h1>index</h1>
+  </view>
 </template>
+
+<script setup lang="ts">
+import CustomNavbar from '@/components/CustomNavbar.vue'
+</script>
 
 <style lang="scss">
 //
