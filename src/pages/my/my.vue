4678ad2f34b98ee61599216408497e53716e2946
Leon <geek_leon@outlook.com>
2024-05-25 17:15:53 +0800

    feat:封装 uni.request 请求

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-import { useMemberStore } from '@/stores'
-
-const memberStore = useMemberStore()
-</script>
-
 <template>
   <view class="my">
     <view>会员信息：{{ memberStore.profile }}</view>
@@ -11,6 +5,7 @@ const memberStore = useMemberStore()
       @tap="
         memberStore.setProfile({
           nickname: '黑马先锋',
+          token: '12345'
         })
       "
       size="mini"
@@ -19,9 +14,27 @@ const memberStore = useMemberStore()
     >
       保存用户信息
     </button>
-    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">
+      清理用户信息
+    </button>
+    <button @tap="fetchData" size="mini" plain type="primary">测试请求</button>
   </view>
 </template>
+
+<script setup lang="ts">
+import { useMemberStore } from '@/stores'
+import { http } from '@/utils/http'
+
+const memberStore = useMemberStore()
+
+const fetchData = async () => {
+  const res = await http<object[]>({
+    url: '/member/profile',
+    method: 'GET'
+  })
+  console.log(res.result)
+}
+</script>
 
 <style lang="scss">
 //
---
+++ src/utils/http.ts
@@ -0,0 +1,77 @@
+import { useMemberStore } from '@/stores'
+
+// 基础地址
+const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
+
+// 拦截器配置
+const httpInterceptor = {
+  invoke(options: UniApp.RequestOptions) {
+    // 拼接baseURL地址
+    if (!options.url.includes('http')) {
+      options.url = baseURL + options.url
+    }
+    // 设置超时时间
+    options.timeout = 10000
+    // 添加小程序请求头标识
+    options.header = {
+      ...options,
+      'source-client': 'miniapp'
+    }
+    // 添加 token 标识
+    const memberStore = useMemberStore()
+    const token = memberStore.profile?.token
+    if (token) {
+      options.header.Authorization = token
+    }
+  }
+}
+
+// 拦截 request 请求
+uni.addInterceptor('request', httpInterceptor)
+// 拦截 uploadFile 请求
+uni.addInterceptor('uploadFile', httpInterceptor)
+
+interface Data<T> {
+  code: string
+  msg: string
+  result: T
+}
+
+export const http = <T>(options: UniApp.RequestOptions) => {
+  return new Promise<Data<T>>((resolve, reject) => {
+    uni.request({
+      ...options,
+      // 响应成功
+      success(res) {
+        // 响应成功状态码
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          resolve(res.data as Data<T>)
+        } else if (res.statusCode === 401) {
+          // 401错误 -> 清空会员信息 -> 跳转到登录页面
+          const memberStore = useMemberStore()
+          memberStore.clearProfile()
+          uni.navigateTo({
+            url: '/pages/login/login'
+          })
+          reject(res)
+        } else {
+          // 其他错误 -> 后端返回的错误
+          uni.showToast({
+            icon: 'none',
+            title: (res.data as Data<T>).msg || '网络请求失败'
+          })
+          reject(res)
+        }
+      },
+      //响应失败
+      fail(err) {
+        // 网络故障
+        uni.showToast({
+          icon: 'none',
+          title: '网络错误!'
+        })
+        reject(err)
+      }
+    })
+  })
+}
