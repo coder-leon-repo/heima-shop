9c3e3eef012add74dbead4c2f52d8a83d39f8ed6
Leon <geek_leon@outlook.com>
2024-05-28 18:24:35 +0800

    refactor:修改对uni.request的封装

---
+++ src/config/service.ts
@@ -0,0 +1,11 @@
+/** 请求基础地址 */
+export const BASE_URL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
+
+/** 请求超时时间 */
+export const REQUEST_TIMEOUT = 10 * 1000
+
+/** 默认的请求错误文本 */
+export const DEFAULT_REQUEST_ERROR_MSG = '请求错误~'
+
+/** 网络不可用的错误文本 */
+export const NETWORK_ERROR_MSG = '网络不可用~'
