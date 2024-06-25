01ccfbf1f967884244f479eab314d9727441b783
Leon <geek_leon@outlook.com>
2024-06-25 23:11:06 +0800

    feat:下拉加载更多数据

--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -1,7 +1,12 @@
 import 'vue'
 import XtxSwiper from '@/components/XtxSwiper.vue'
+import XtxGuess from '@/components/XtxGuess.vue'
+
 declare module 'vue' {
   export interface GlobalComponents {
     XtxSwiper: typeof XtxSwiper
+    XtxGuess: typeof XtxGuess
   }
 }
+
+export type XtxGuessInstance = InstanceType<typeof XtxGuess>
--- src/types/home.d.ts
+++ src/types/home.d.ts
@@ -1,37 +1,28 @@
-/** 首页-广告区域*/
+import type { GoodsItem } from "./global"
+
+// 首页-广告区域
 export type BannerItem = {
-  /** 跳转链接 */
   hrefUrl: string
-  /** id */
   id: string
-  /** 图片链接 */
   imgUrl: string
-  /** 跳转类型 */
   type: number
 }
 
-/** 首页-前台分类*/
+// 首页-前台分类
 export type CategoryItem = {
-  /** 图标路径 */
   icon: string
-  /** id */
   id: string
-  /** 分类名称 */
   name: string
 }
 
 // 首页-热门推荐
 export type HotItem = {
-  // id
   id: string
-  //推荐说明
   alt: string
-  // 图片集合
   pictures: string[]
-  // 跳转地址
   target: string
-  // 推荐标题
   title: string
-  // 推荐类型
   type: number
 }
+
+export type GuessItem = GoodsItem
